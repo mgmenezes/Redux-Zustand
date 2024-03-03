a8ca55f679526ec39582cb7f8c10822901dc4b06
Moisés Menezes <moisesmenezes53@gmail.com>
2024-03-02 21:18:57 -0300

    feat: Creating Global Hook useCurrentLesson

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,19 +1,13 @@
 import ReactPlayer from "react-player";
 import { useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
-import { next } from "../store/slices/player";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export default function Video() {
   const dispatch = useDispatch();
 
-  const lesson = useAppSelector((state) => {
-    const { currentLessonIndex, currentModuleIndex } = state.player;
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[
-        currentLessonIndex
-      ];
-    return currentLesson;
-  });
+  const { currentLesson } = useCurrentLesson();
+
   function handlePlayNext() {
     dispatch(next());
   }
@@ -25,7 +19,7 @@ export default function Video() {
           height="100%"
           controls
           playing
-          url={`https://www.youtube.com/watch?v=${lesson.id}`}
+          url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
           onEnded={handlePlayNext}
         />
       </div>
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,11 +3,18 @@ import Header from "../components/Header";
 import Video from "../components/Video";
 import Module from "../components/Module";
 import { useAppSelector } from "../store";
+import { useCurrentLesson } from "../store/slices/player";
+import { useEffect } from "react";
 
 export default function Player() {
   const modules = useAppSelector((state) => {
     return state.player.course.modules;
   });
+  const { currentLesson } = useCurrentLesson();
+
+  useEffect(() => {
+    document.title = `Assistindo: ${currentLesson}`;
+  }, [currentLesson]);
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
